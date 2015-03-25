c77085f800c73ed204a8cd88573f3d5677c6fcdf
Jillian Tullo <tullo_jillian@bah.com>
2015-03-25 09:53:22 -0500

    - database additions

--- spec/models/jellyfish/fog/azure/database_product_spec.rb
+++ spec/models/jellyfish/fog/azure/database_product_spec.rb
@@ -5,9 +5,17 @@ module Jellyfish
     module Azure
       describe DatabaseProduct do
         mock = Jellyfish::Fog::Azure::Mock.new
+        let(:order_item) { double('order item', id: 1, answers: { location: 'East Asia' }) }
         it 'returns an appropriate provisioner' do
           mock.mock!
           expect(DatabaseProduct.new.provisioner).to eq(Databases)
+        end
+
+        it 'creates a new database server' do
+          mock.mock!
+          order_item.should_receive(:provision_status=).with(:ok)
+          order_item.should_receive(:payload_response=).with a_kind_of(Hash)
+          Databases.new(order_item).provision
         end
       end
     end
