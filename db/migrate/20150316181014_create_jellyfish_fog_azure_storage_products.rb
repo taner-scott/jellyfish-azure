ba47a18b8772d87d9895e34c78badf7424d56925
Jillian Tullo <tullo_jillian@bah.com>
2015-03-26 17:00:32 -0500

    removed the database migrations

--- db/migrate/20150316181014_create_jellyfish_fog_azure_storage_products.rb
+++
@@ -1,8 +0,0 @@
-class CreateJellyfishFogAzureStorageProducts < ActiveRecord::Migration
-  def change
-    create_table :jellyfish_fog_azure_storage_products do |t|
-      t.timestamp :timestamp
-      t.string :location
-    end
-  end
-end
