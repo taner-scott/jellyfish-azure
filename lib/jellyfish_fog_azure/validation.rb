d96b3e45ccf47d60379f3aac76ac488f9dfa9969
Jillian Tullo <tullo_jillian@bah.com>
2015-03-27 12:58:05 -0500

    - updated the validation script to re-write the infrastructure.json with the images when the credentials are validated

--- lib/jellyfish_fog_azure/validation.rb
+++ lib/jellyfish_fog_azure/validation.rb
@@ -8,7 +8,6 @@ module Jellyfish
         def validate
           server_list = Connection.new.connect.list_images
           valid = false
-          # puts "Your server list stuff: #{server_list}"
           # Can return nil if no servers are present.
           # Will return an error if the connection is incorrect
           # Ensuring it is a valid test
@@ -23,19 +22,15 @@ module Jellyfish
         def save_images_json images
           json_array = []
           images.each do |image|
-            hash = {
-              name: image.name,
-              os_type: image.os_type,
-              category: image.category
-            }
+            hash = image.name
             json_array << hash
           end
-          File.open("doc/images.json","w") do |f|
-            f.write("\n")
-            json_array.each_with_index do |json_image, index|
-              f.write(json_image.to_json)
-              index == json_array.size - 1 ? f.write("\n") : f.write(",\n")
-            end
+          file_name = File.expand_path('../../../infrastructure.json', __FILE__)
+          json_file = File.read(file_name)
+          data_hash = JSON.parse(json_file)
+          data_hash['properties']['image']['enum'] = json_array
+          File.open(file_name, "w") do |f|
+            f.write(data_hash.to_json)
           end
         end
       end
--- spec/models/jellyfish/fog/azure/validation_spec.rb
+++ spec/models/jellyfish/fog/azure/validation_spec.rb
@@ -4,15 +4,10 @@ module Jellyfish
   module Fog
     module Azure
       describe Validation do
-        ENV['JF_AZURE_SUB_ID'] = 'abcdefg'
+        ENV['JF_AZURE_SUB_ID'] = 'fake'
         ENV['JF_AZURE_PEM_PATH'] = 'fake.pem'
         ENV['JF_AZURE_API_URL'] = 'https://management.core.windows.net'
         mock = Jellyfish::Fog::Azure::Mock.new
-        it 'returns a true value for valid credentials' do
-          mock.mock!
-          validated = Validation.new.validate
-          expect(validated).to eq(true)
-        end
 
         it 'returns a false value for invalid credentials' do
           mock.unmock!
