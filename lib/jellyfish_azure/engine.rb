5e874d321e9c4cec14898550dc18e321c13cca6a
Taner <taner.scott@neudesic.com>
2015-10-02 11:23:53 -0700

    modified jellyfish_azure engine.rb so that it would make the correct calls

--- lib/jellyfish_azure/engine.rb
+++ lib/jellyfish_azure/engine.rb
@@ -11,7 +11,7 @@ module JellyfishAzure
       app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
     end
 
-    initializer 'jellyfish_azure.load_registered_providers', before: load_config_initializers do
+    initializer 'jellyfish_azure.load_registered_providers', :before => :load_config_initializers do
       begin
         if RegisteredProvider.table_exists?
           Dir[File.expand_path '../../../app/models/jellyfish_azure/registered_provider/*', __FILE__].each do |file|
@@ -24,7 +24,7 @@ module JellyfishAzure
       end
     end
 
-    initializer 'jellyfish_azure.load_product_types', before: load_config_initializers do
+    initializer 'jellyfish_azure.load_product_types', :before => :load_config_initializers do
       begin
         if ProductType.table_exists?
           Dir[File.expand_path '../../../app/models/jellyfish_azure/product_type/*.rb', __FILE__].each do |file|
@@ -37,7 +37,7 @@ module JellyfishAzure
       end
     end
 
-    initializer 'jellyfish_azure.register_extension', after: finisher_hook do ||
+    initializer 'jellyfish_azure.register_extension', :after => :finisher_hook do ||
       ::Jellyfish::Extension.register 'jellyfish-azure' do
         requires_jellyfish '>= 4.0.0'
 
