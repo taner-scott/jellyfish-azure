38ddae16648bcd5ebeebb313be9cb0613934df85
Chris Predeek <predeekc@hotmail.com>
2015-10-08 13:19:44 -0700

    Added template deployment support to a webdev environment product type

---
+++ app/models/jellyfish_azure/service/azure_service.rb
@@ -0,0 +1,69 @@
+module JellyfishAzure
+  module Service
+    class AzureService < ::Service::Storage
+
+      def client
+        @_client ||= begin
+          result = Azure::ARM::Resources::ResourceManagementClient.new product.provider.credentials
+          result.subscription_id = product.provider.subscription_id
+          result
+        end
+      end
+
+      def resource_group_name
+        @_resource_group_name ||= begin
+          short_uuid = uuid.tr '-', ''
+          ("jf#{short_uuid}_#{name}")[0...85]
+        end
+      end
+
+      def ensure_resource_group location
+        resource_group = Azure::ARM::Resources::Models::ResourceGroup.new()
+        resource_group.location = location
+
+        promise = client.resource_groups.create_or_update(resource_group_name, resource_group)
+        promise.value!
+
+        resource_group_name
+      end
+
+      def deploy_template(deployment_name, template_url, template_parameters)
+        logger.info "Starting '#{deployment_name}' in resource group '#{resource_group_name}'"
+
+        properties = Azure::ARM::Resources::Models::DeploymentProperties.new
+        properties.mode = 'Incremental'
+        properties.parameters = template_parameters
+
+        # TODO: replace this with the template url once it's working well
+        properties.template = JSON.parse(File.read(template_url));
+
+        params = Azure::ARM::Resources::Models::Deployment.new
+        params.properties = properties;
+
+        promise = client.deployments.create_or_update(resource_group_name, deployment_name, params);
+        promise.value!
+      end
+
+      def monitor_deployment(deployment_name)
+        done = false
+        attempts = 0
+
+        while attempts < 240 and not done
+
+          attempts += 1
+
+          promise = client.deployments.get resource_group_name, deployment_name
+          result = promise.value!
+          state = result.body.properties.provisioning_state
+
+          done = (state != "Accepted" and state != "Running")
+
+          sleep(30) if not done
+        end
+
+        # TODO: handle deployment timeout
+        Hash[result.body.properties.outputs.map { |key, value| [ key, value['value'] ] }]
+      end
+    end
+  end
+end
