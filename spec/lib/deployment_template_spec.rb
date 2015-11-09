36eca5f140468c376e7f251e847d7bf06bd3b0a0
Chris Predeek <predeekc@hotmail.com>
2015-11-09 08:58:18 -0800

    Simplified deployment template spec

--- spec/lib/deployment_template_spec.rb
+++ spec/lib/deployment_template_spec.rb
@@ -3,87 +3,39 @@ require 'jellyfish_azure'
 
 module JellyfishAzure
   describe DeploymentTemplate do
-    describe '#parameters' do
-      context 'when parameters are parsed' do
-        let(:template) { arm_template :simple_template }
+    shared_examples_for 'template_parameter' do |index, name, type, field, default_value, allowed_values = nil|
+      context "parameter #{index}" do
+        subject { template.parameters[index] }
 
-        it 'there are two parameters' do
-          expect(template.parameters.length).to eq 2
+        it "name should eq '#{name}'" do
+          expect(subject.name).to eq name
         end
-
-        it 'the parameter names are valid' do
-          expect(template.parameters[0].name).to eq 'param1'
-          expect(template.parameters[1].name).to eq 'param2'
+        it "type should eq '#{type}'" do
+          expect(subject.type).to eq type
         end
-
-        it 'the default value is set' do
-          expect(template.parameters[0].default_value).to eq 'default'
+        it "field should eq '#{field}'" do
+          expect(subject.field).to eq field
         end
-
-        it 'the default value is nil' do
-          expect(template.parameters[1].default_value).to eq nil
+        it "default_value should eq '#{default_value}'" do
+          expect(subject.default_value).to eq default_value
         end
-
-        it 'the required value is true' do
-          expect(template.parameters[1].required).to be true
-        end
-
-        it 'the required value is false' do
-          expect(template.parameters[0].required).to be false
-        end
-
-        it 'the first parameter has allowed values' do
-          expect(template.parameters[0].allowed_value).to eq %w(one two three)
-        end
-
-        it 'the second param has no allowed values' do
-          expect(template.parameters[1].allowed_value).to be nil
-        end
-      end
-
-      context 'when parameter types are parsed' do
-        let(:template) { arm_template :all_types_template }
-
-        it 'the stringParam is of type string' do
-          expect(template.parameters[0].type).to eq :string
-        end
-
-        it 'the securestringParam is of type string' do
-          expect(template.parameters[1].type).to eq :string
-        end
-
-        it 'the intParam is of type integer' do
-          expect(template.parameters[2].type).to eq :integer
-        end
-
-        it 'the boolParam is of type boolean' do
-          expect(template.parameters[3].type).to eq :boolean
-        end
-      end
-
-      context 'when parameter field types are parsed' do
-        let(:template) { arm_template :all_types_template }
-
-        it 'the stringParam has field type text' do
-          expect(template.parameters[0].field).to eq :text
-        end
-
-        it 'the securestringParam has field type password' do
-          expect(template.parameters[1].field).to eq :password
-        end
-
-        it 'the intParam has field type az_integer' do
-          expect(template.parameters[2].field).to eq :az_integer
-        end
-
-        it 'the boolParam has field type checkbox' do
-          expect(template.parameters[3].field).to eq :checkbox
-        end
-
-        it 'the stringChoiceParam has field type az_choice' do
-          expect(template.parameters[4].field).to eq :az_choice
+        it "allowed_values should eq '#{allowed_values}'" do
+          expect(subject.allowed_value).to eq allowed_values
         end
       end
     end
+
+    let(:template) { arm_template :all_types_template }
+
+    context 'parameter count' do
+      subject { template.parameters.length }
+      it { is_expected.to eq 5 }
+    end
+
+    it_should_behave_like 'template_parameter', 0, 'stringParam', :string, :text, nil
+    it_should_behave_like 'template_parameter', 1, 'securestringParam', :string, :password, nil
+    it_should_behave_like 'template_parameter', 2, 'intParam', :integer, :az_integer, nil
+    it_should_behave_like 'template_parameter', 3, 'boolParam', :boolean, :checkbox, nil
+    it_should_behave_like 'template_parameter', 4, 'stringChoiceParam', :string, :az_choice, nil, %w(one two)
   end
 end
